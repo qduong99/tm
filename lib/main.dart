50eda6f623db2ae6779c529fb67fbc44148124b0
qduong99 <duongnguyenquang@gmail.com>
2020-11-19 10:27:10 -0600

    clean up.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:TM/src/notifier/AppState.dart';
 import 'package:TM/src/screens/LoginScreen.dart';
 import 'package:TM/src/theme.dart';
 import 'package:flutter/material.dart';
@@ -18,9 +17,6 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => AppState()),
-        ],
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: appTheme,

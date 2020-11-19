46c7ad806d44bb9a1ad1710105f70dfc8fe96ec8
Marcelo Sotomaior <marcelo.sotomaior@gmail.com>
2020-11-19 15:02:09 -0600

    Videos playing dinamically
    Implemented dynamic videos

--- lib/src/page/VideosDetailPage.dart
+++ lib/src/page/VideosDetailPage.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:TM/src/common/widget/TMImage.dart';
 import 'package:TM/src/theme.dart';
 import 'package:flutter/material.dart';
@@ -25,19 +27,28 @@ class _VideosDetailPageState extends State<VideosDetailPage> {
           Text("Video"),
         ]),
       ),
-      body: CustomMainPage(),
+      body: CustomMainPage(widget.principalId),
     );
   }
 }
 
 class CustomMainPage extends StatefulWidget {
-  CustomMainPage();
+
+  final int videosId;
+  CustomMainPage(this.videosId);
 
   @override
   _CustomMainPageState createState() => _CustomMainPageState();
 }
 
 class _CustomMainPageState extends State<CustomMainPage> {
+
+
+
+  Map<int, List<String>> images = new HashMap();
+
+  Map<int, List<String>> videos = new HashMap();
+
   final _formKey = GlobalKey<FormState>();
 
   bool _btnNext = true;
@@ -58,10 +69,46 @@ class _CustomMainPageState extends State<CustomMainPage> {
     });
   }
 
+  @override
+  initState() {
+    super.initState();
+    images[1] = ["principals/1/1.png", "principals/1/2.png", "principals/1/3.png"];
+    images[2] = ["principals/2/1.png", "principals/2/2.png", "principals/2/3.png", "principals/2/4.png"];
+    images[3] = ["principals/3/1.png", "principals/3/2.png", "principals/3/3.png"];
+    images[4] = ["principals/4/1.png"];
+    videos[1] = ["https://www.youtube.com/watch?v=RJ44oIxWiYI"];
+    videos[2] = ["https://www.youtube.com/watch?v=V3WkgVpv1Zk"];
+    videos[3] = ["https://www.youtube.com/watch?v=Yq5C1Qt4jPE"];
+    videos[4] = ["https://www.youtube.com/watch?v=z6A-2U6wGw8"];
+  }
+
+
+  _build() {
+    List<Widget> result = [];
+    List<String> principalImages = images[widget.videosId];
+    for (String imagePath in principalImages) {
+      result.add(new TMImage(image: imagePath));
+
+      result.add(SizedBox(height: 8));
+    }
+    return ListView(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: result,
+    );
+  }
+
   void playYoutubeVideo() {
+    List<Widget> result = [];
+    List<String> principalVideos = videos[widget.videosId];
+    String url;
+    for (String videoPath in principalVideos) {
+      url = videoPath;
+    }
+
     FlutterYoutube.playYoutubeVideoByUrl(
       apiKey: "<API_KEY>",
-      videoUrl: "https://www.youtube.com/watch?v=RJ44oIxWiYI",
+      videoUrl: url,
     );
   }
 
@@ -84,7 +131,7 @@ class _CustomMainPageState extends State<CustomMainPage> {
                       children: <Widget>[
                         // UnloadBy(),
                         SizedBox(height: 8),
-                        TMImage(image: "videos/1.jpg"),
+                        _build(),
                         SizedBox(height: 8),
                         Padding(
                           padding: const EdgeInsets.symmetric(vertical: 2.0),
--- lib/src/page/home/tabs/VideosTab.dart
+++ lib/src/page/home/tabs/VideosTab.dart
@@ -1,15 +1,20 @@
+import 'package:TM/src/page/PrincipalDetailPage.dart';
 import 'package:TM/src/page/VideosDetailPage.dart';
 import 'package:flutter/material.dart';
-
 import '../../../common/widget/ItemIcon.dart';
 import '../../VideosDetailPage.dart';
 
 class VideosTab extends StatefulWidget {
+
   @override
   _VideosTabState createState() => _VideosTabState();
 }
 
 class _VideosTabState extends State<VideosTab> {
+
+  var video1 = 'Asana';
+  var video2 = 'TM Videos';
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,6 +26,7 @@ class _VideosTabState extends State<VideosTab> {
         padding: const EdgeInsets.all(8),
         child: GridView.count(
           //to avoid scrolling conflict with the dragging sheet
+
           physics: NeverScrollableScrollPhysics(),
           padding: const EdgeInsets.all(0),
           crossAxisCount: 3,
@@ -28,9 +34,10 @@ class _VideosTabState extends State<VideosTab> {
           crossAxisSpacing: 6,
           shrinkWrap: true,
           children: <Widget>[
-            ItemIcon(text: Text('TM Videos'), iconData: Icons.check_box, onTap: _onGIPlanned),
+            ItemIcon(text: Text(video1), iconData: Icons.add_a_photo, onTap:() {_onGIPlanned(1);}),
+            ItemIcon(text: Text(video2), iconData: Icons.chat, onTap:() {_onGIPlanned(2);}),
             ItemIcon(text: Text('TM Resources'), iconData: Icons.check_box, onTap: (){
-              _onGIPlanned();
+              _onGIPlanned(3);
             }),
           ],
         ),
@@ -38,15 +45,17 @@ class _VideosTabState extends State<VideosTab> {
     );
   }
 
-  void _onGIPlanned() async {
+  void _onGIPlanned(int videosId) async {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (BuildContext context) {
-          return VideosDetailPage(1);
+          return VideosDetailPage(videosId);
         },
       ),
     );
   }
+
+
 
   void _onGIUnplanned() async {
     Navigator.of(context).pushNamed(
